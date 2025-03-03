08e62d80393e84c91169b651b96cfd7084481c91
DavidValencia526 <davidvalencia0526@gmail.com>
2025-03-03 17:26:02 -0500

    add missing indexes

--- cleanDataBase.sql
+++ cleanDataBase.sql
@@ -794,3 +794,37 @@ CREATE INDEX idx_status_type_status_type_id ON status_type USING btree (status_t
 CREATE INDEX idx_transaction_category_id ON transaction_category USING btree (transaction_category_id);
 
 CREATE INDEX idx_transaction_type_id ON transaction_type USING btree (transaction_type_id);
+
+CREATE INDEX idx_invitation_inviter_user_id ON invitation USING btree (inviter_user_id);
+
+CREATE INDEX idx_health_checks_status_id ON health_checks USING btree (status_id);
+
+CREATE INDEX idx_health_checks_recommendation_id ON health_checks USING btree (recommendation_id);
+
+CREATE INDEX idx_health_checks_cultural_work_tasks_id ON health_checks USING btree (cultural_work_tasks_id);
+
+CREATE INDEX idx_cultural_work_tasks_plot_id ON cultural_work_tasks USING btree (plot_id);
+
+CREATE INDEX idx_cultural_work_tasks_cultural_works_id ON cultural_work_tasks USING btree (cultural_works_id);
+
+CREATE INDEX idx_notifications_invitation_id ON notifications USING btree (invitation_id);
+
+CREATE INDEX idx_notifications_farm_id ON notifications USING btree (farm_id);
+
+CREATE INDEX idx_notifications_status_id ON notifications USING btree (status_id);
+
+CREATE INDEX idx_transaction_creador_id ON transaction USING btree (creador_id);
+
+CREATE INDEX idx_flowering_flowering_type_id ON flowering USING btree (flowering_type_id);
+
+CREATE INDEX idx_farm_status_id ON farm USING btree (status_id);
+
+CREATE INDEX idx_users_email ON users USING btree (email);
+
+CREATE INDEX idx_transaction_category_transaction_type_id ON transaction_category USING btree (transaction_type_id);
+
+CREATE INDEX idx_cultural_work_tasks_plot_status ON cultural_work_tasks USING btree (plot_id, status_id);
+
+CREATE INDEX idx_transaction_plot_date ON transaction USING btree (plot_id, transaction_date);
+
+CREATE INDEX idx_user_role_farm_user_role ON user_role_farm USING btree (user_id, role_id);
