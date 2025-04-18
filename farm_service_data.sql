b55d6dc0943d43a68d34124a5d79c89a4e3263a4
DavidValencia526 <davidvalencia0526@gmail.com>
2025-04-18 03:25:46 -0500

    refactor farm service

--- farm_service_data.sql
+++ farm_service_data.sql
@@ -21,21 +21,15 @@ INSERT INTO unit_of_measure (unit_of_measure_id, name, abbreviation, unit_of_mea
 (8, 'Metro cuadrado', 'm²', 2),
 (9, 'Kilómetro cuadrado', 'km²', 2);
 
--- Inserción de tipos de estado
-INSERT INTO status_type (status_type_id, name) VALUES 
-(1, 'Farm');
+-- Inserción de estados de finca
+INSERT INTO farm_status (farm_status_id, name) VALUES 
+(1, 'Activo'),
+(2, 'Inactivo');
 
--- Inserción de estados
-INSERT INTO status (status_id, name, status_type_id) VALUES 
-(1, 'Activo', 1),
-(2, 'Inactivo', 1);
 
 -- Actualización de secuencias
-SELECT setval(pg_get_serial_sequence('status', 'status_id'), 
-              (SELECT COALESCE(MAX(status_id), 0) + 1 FROM status), false);
-
-SELECT setval(pg_get_serial_sequence('status_type', 'status_type_id'), 
-              (SELECT COALESCE(MAX(status_type_id), 0) + 1 FROM status_type), false);
+SELECT setval(pg_get_serial_sequence('farm_status', 'farm_status_id'), 
+              (SELECT COALESCE(MAX(farm_status_id), 0) + 1 FROM farm_status), false);
 
 SELECT setval(pg_get_serial_sequence('unit_of_measure_type', 'unit_of_measure_type_id'), 
               (SELECT COALESCE(MAX(unit_of_measure_type_id), 0) + 1 FROM unit_of_measure_type), false);
