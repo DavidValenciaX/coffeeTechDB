b8d7e8a1d1d9d7be1494702f812263fd64c9a11d
DavidValencia526 <davidvalencia0526@gmail.com>
2025-04-18 14:44:55 -0500

    reordered permission ids

--- user_service_data.sql
+++ user_service_data.sql
@@ -1,14 +1,14 @@
 BEGIN;
 
 -- Inserción de tipos de estado relevantes para el servicio de usuarios
-INSERT INTO public.status_type (status_type_id, name) VALUES
+INSERT INTO status_type (status_type_id, name) VALUES
 (1, 'User'),
 (2, 'Notification'),
 (3, 'Invitation'),
 (4, 'user_role_farm');
 
 -- Inserción de estados relevantes para el servicio de usuarios
-INSERT INTO public.status (status_id, name, status_type_id) VALUES
+INSERT INTO status (status_id, name, status_type_id) VALUES
 -- User statuses
 (1, 'Activo', 1),
 (2, 'Verificado', 1),
@@ -31,47 +31,47 @@ INSERT INTO public.status (status_id, name, status_type_id) VALUES
 (13, 'Inactivo', 4);
 
 -- Inserción de tipos de notificación
-INSERT INTO public.notification_type (notification_type_id, name) VALUES
+INSERT INTO notification_type (notification_type_id, name) VALUES
 (1, 'Invitation'),
 (2, 'Reminder'),
 (3, 'Invitation_Accepted'),
 (4, 'Invitation_Rejected');
 
 -- Inserción de permisos
-INSERT INTO public.permission (permission_id, description, name) VALUES
+INSERT INTO permission (permission_id, description, name) VALUES
 (1, 'Permite al usuario modificar la informacion de la finca', 'edit_farm'),
-(5, 'Permite al usuario eliminar finca de donde es propietario', 'delete_farm'),
+(2, 'Permite al usuario eliminar finca de donde es propietario', 'delete_farm'),
 
-(2, 'Permite al usuario agregar administrador de finca', 'add_administrator_farm'),
-(11, 'Permite al usuario editar un rol de colaborador a administrador de finca', 'edit_administrator_farm'),
-(6, 'Permite al usuario eliminar administrador de finca', 'delete_administrator_farm'),
+(3, 'Permite al usuario agregar administrador de finca', 'add_administrator_farm'),
+(4, 'Permite al usuario editar un rol de colaborador a administrador de finca', 'edit_administrator_farm'),
+(5, 'Permite al usuario eliminar administrador de finca', 'delete_administrator_farm'),
 
-(3, 'Permite al usuario agregar operador de campo', 'add_operator_farm'),
-(10, 'Permite al usuario editar un rol de colaborador a operador de campo', 'edit_operator_farm'),
-(7, 'Permite al usuario eliminar operador de campo', 'delete_operator_farm'),
+(6, 'Permite al usuario agregar operador de campo', 'add_operator_farm'),
+(7, 'Permite al usuario editar un rol de colaborador a operador de campo', 'edit_operator_farm'),
+(8, 'Permite al usuario eliminar operador de campo', 'delete_operator_farm'),
 
 (9, 'Permite al usuario listar los colaboradores de una finca', 'read_collaborators'),
 
-(4, 'Permite al usuario agregar lotes', 'add_plot'),
-(12, 'Permiso para editar lotes', 'edit_plot'),
-(13, 'Permiso para eliminar lotes', 'delete_plot'),
-(8, 'Permite al usuario listar los lotes', 'read_plots'),
+(10, 'Permite al usuario agregar lotes', 'add_plot'),
+(11, 'Permiso para editar lotes', 'edit_plot'),
+(12, 'Permiso para eliminar lotes', 'delete_plot'),
+(13, 'Permite al usuario listar los lotes', 'read_plots'),
 
-(24, 'Permite al usuario ver las transaciones de la finca', 'read_transaction'),
-(25, 'Permite al usuario editar las transaciones de la finca', 'edit_transaction'),
-(26, 'Permite al usuario agregar las transaciones de la finca', 'add_transaction'),
-(27, 'Permite al usuario eliminar las transaciones de la finca', 'delete_transaction'),
+(14, 'Permite al usuario ver las transaciones de la finca', 'read_transaction'),
+(15, 'Permite al usuario editar las transaciones de la finca', 'edit_transaction'),
+(16, 'Permite al usuario agregar las transaciones de la finca', 'add_transaction'),
+(17, 'Permite al usuario eliminar las transaciones de la finca', 'delete_transaction'),
 
-(29, 'Permite al usuario ver los reportes financieros', 'read_financial_report');
+(18, 'Permite al usuario ver los reportes financieros', 'read_financial_report');
 
 -- Inserción de roles
-INSERT INTO public.role (role_id, name) VALUES
+INSERT INTO role (role_id, name) VALUES
 (1, 'Propietario'),
 (2, 'Administrador de finca'),
 (3, 'Operador de campo');
 
 -- Inserción de permisos por rol
-INSERT INTO public.role_permission (role_id, permission_id) VALUES
+INSERT INTO role_permission (role_id, permission_id) VALUES
 (1, 1),
 (1, 2),
 (1, 3),
@@ -85,32 +85,27 @@ INSERT INTO public.role_permission (role_id, permission_id) VALUES
 (1, 11),
 (1, 12),
 (1, 13),
-
-(1, 24),
-(1, 25),
-(1, 26),
-(1, 27),
-
-(1, 29),
+(1, 14),
+(1, 15),
+(1, 16),
+(1, 17),
+(1, 18),
 
 
 (2, 1),
 
-(2, 3),
-(2, 4),
-
+(2, 6),
 (2, 7),
 (2, 8),
 (2, 9),
 (2, 10),
-
+(2, 11),
 (2, 12),
 (2, 13),
-
-(2, 24),
-(2, 25),
-(2, 26),
-(2, 27);
+(2, 14),
+(2, 15),
+(2, 16),
+(2, 17);
 
 -- Actualización de secuencias para las tablas pobladas
 SELECT setval(pg_get_serial_sequence('status_type', 'status_type_id'),
